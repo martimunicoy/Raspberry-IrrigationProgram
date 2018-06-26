e0c9216bc79d7b73fbc7955232b750973fdd5164
Marti Municoy <martimunicoy@gmail.com>
2018-06-26 19:15:33 +0200

    Most recent version

--- IrrigationProgram.py
+++ IrrigationProgram.py
@@ -1,75 +1,60 @@
 import constants as co
-import getopt
-import sys
-import timing
-import time
-import logging
-
+import logging as log
 from electrovalve import Electrovalve
-from terminal_interface import TerminalInterface
-from mailing import Mail
-
+from terminal_interface import TerminalInterface, StatusBulb
 from optparse import OptionParser
-from threading import Timer
+from timing import Schedule, initiate_timer
 
-no_interface = True
 
 def main():
-	parser = OptionParser()
-	parser.add_option("-f", "--file", dest="sch_dir", help="directory of the Schedule file", metavar="FILE_DIRECTORY")
-	parser.add_option("-t", "--test", dest="test", action='store_true', help="functional test")
-	parser.add_option("-s", "--simulation", dest="sim", action='store_true', help='simulation mode where actually no relay is turned on')
-	parser.add_option("-l", "--log", dest="log_dir", help="directory of the log file", metavar="FILE_DIRECTORY")
-	args_dict = parser.parse_args()[0].__dict__
+    mystatusbulb = StatusBulb()
+    mystatusbulb.open()
 
-	log_dir = args_dict['log_dir']
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="sch_dir",
+                      help="directory of the Schedule file",
+                      metavar="FILE_DIRECTORY")
+    parser.add_option("-l", "--log", dest="log_level",
+                      help="set the logging level",
+                      metavar="DEBUG, INFO, WARNING, ERROR, CRITICAL")
+    parser.add_option("-t", "--test", dest="test", action='store_true',
+                      help="functional test")
+    parser.add_option("-s", "--simulation", dest="sim",
+                      action='store_true',
+                      help='simulation mode where actually no ' +
+                           'relay is turned on')
+    parser.add_option("-d", "--log_dir", dest="log_dir",
+                      help="directory of the log file",
+                      metavar="FILE_DIRECTORY")
+    args_dict = parser.parse_args()[0].__dict__
 
-	if log_dir is None:
-		log_dir = 'IrrigationProgram.log'
+    log_dir = args_dict['log_dir']
+    if log_dir is None:
+        log_dir = 'IrrigationProgram.log'
+    log_level = args_dict['log_level']
+    if log_level is None:
+        log_level = 'INFO'
+    log.basicConfig(filename=log_dir, level=log_level,
+                    format='%(asctime)s %(message)s')
+    log.info(co.SEPARATOR)
+    log.info(co.MAIN_INFO)
 
-	logging.basicConfig(filename=log_dir)
+    kwargs = {}
+    for key, item in args_dict.iteritems():
+        if item is True:
+            kwargs[key] = item
 
-	kwargs = {}
-	
-	for key, item in args_dict.iteritems():
-		if item == True:
-			kwargs[key] = item
-	
-	sch_dir = args_dict['sch_dir']
+    sch_dir = args_dict['sch_dir']
 
-	myschedule = timing.Schedule(sch_dir, **kwargs)
+    myschedule = Schedule(sch_dir, **kwargs)
 
-	myelectrovalve = Electrovalve(pin=27, **kwargs)
+    myelectrovalve = Electrovalve(myschedule, pin=27, **kwargs)
 
-	timer(myschedule, myelectrovalve, **kwargs)
+    myinterface = TerminalInterface(myschedule, myelectrovalve, mystatusbulb,
+                                    **kwargs)
 
+    initiate_timer(myschedule, myelectrovalve, myinterface, **kwargs)
 
-def timer(schedule, electrovalve, test=False, sim=False):
-	global no_interface
-	delay = schedule.next_timer()[0]
-	t = Timer(delay, water, args=(schedule, electrovalve, test))
-	t.start()
-	if test:
-		print co.TEST_WARNING
-	elif no_interface:
-		myinterface = TerminalInterface(t, schedule)
-		no_interface = False
-
-
-def water(schedule, electrovalve, test):
-	for span in map(float, schedule.cycle):
-		electrovalve.open()
-		time.sleep(span)
-		electrovalve.close()
-		time.sleep(co.GAP)
-
-	mymail = Mail(schedule)
-	mymail.send_success()
-
-	if test:
-		sys.exit(0)
-	# Add a condition to exit if wanted
-	main()
 
 if __name__ == "__main__":
-	main()
+    main()
--- mailing.py
+++ mailing.py
@@ -1,38 +1,38 @@
 import smtplib
 import constants as co
 import timing
-import datetime
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
+
 class Mail(object):
 
-	def __init__(self, schedule, issuer=co.MAIL, psswd=co.MAIL_PSSWD):
-		self.schedule = schedule
-		self.issuer = issuer
-		self.psswd = psswd
-		self.server = smtplib.SMTP('smtp.gmail.com', 587)
+    def __init__(self, schedule, issuer=co.MAIL, psswd=co.MAIL_PSSWD):
+        self.schedule = schedule
+        self.issuer = issuer
+        self.psswd = psswd
+        self.server = smtplib.SMTP('smtp.gmail.com', 587)
 
-		self.server.starttls()
-		self.server.login(issuer, psswd)
+        self.server.starttls()
+        self.server.login(issuer, psswd)
 
+    def send_success(self, receiver='martimunicoy@gmail.com'):
+        msg = MIMEMultipart()
+        msg['From'] = self.issuer
+        msg['To'] = receiver
+        msg['Subject'] = co.MAIL_SUBJECT
 
-	def send_success(self, receiver='martimunicoy@gmail.com'):
-		msg = MIMEMultipart()
-		msg['From'] = self.issuer
-		msg['To'] = receiver
-		msg['Subject'] = co.MAIL_SUBJECT
+        time_now = timing.today()
+        sch_str = ""
+        for hour in self.schedule.hours:
+            sch_str += "\n    {}            {}".format(
+                hour.time, hour.lag_time())
+        cyc_str = ""
+        for output, tm in zip(co.CYCLE_OUTPUTS, self.schedule.cycle):
+            cyc_str += "\n{}         {}".format(output, tm)
+        body = "Hi,\n" + co.MAIL_MSG.format(time_now) +\
+            co.SCHEDULE.format(time_now) + sch_str + "\n" +\
+            co.CYCLE_INFO + cyc_str + co.MAIL_ENDING
+        msg.attach(MIMEText(body, 'plain'))
 
-		time_now = timing.today().replace(microsecond=0)
-		sch_str = ""
-		for delay in self.schedule.next_timer():
-					hour = (datetime.timedelta(seconds=delay) + timing.today()).replace(microsecond=0)
-					remaining = (datetime.datetime(1970,1,1) + datetime.timedelta(seconds=delay)).replace(microsecond=0)
-					sch_str += "\n    {}            {}".format(hour.time(), remaining.time())
-		cyc_str = ""
-		for output, tm in zip(co.CYCLE_OUTPUTS, self.schedule.cycle):
-			cyc_str += "\n{}         {}".format(output, tm)
-		body = "Hi,\n" + co.MAIL_MSG.format(time_now) + co.SCHEDULE.format(time_now) + sch_str + "\n" + co.CYCLE_INFO + cyc_str + co.MAIL_ENDING
-		msg.attach(MIMEText(body, 'plain'))
-
-		self.server.sendmail(self.issuer, receiver, msg.as_string())
+        self.server.sendmail(self.issuer, receiver, msg.as_string())
